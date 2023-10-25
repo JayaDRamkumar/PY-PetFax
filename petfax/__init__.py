bb5211bc542c8878e3ef4be9c78bdbc1881fc84c
“JayaDRamkumar” <jayadramkumar@gmail.com>
2023-10-24 21:02:37 -0400

    part 7

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:honey95@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    from . import models 
+    models.db.init_app(app) 
+    migrate = Migrate(app, models.db)
     @app.route('/')
     def hello(): 
-        return 'Hello, PetFax!'
+            return 'Hello, PetFax!'
 
-   # register pet blueprint 
+    # register pet blueprint 
     from . import pet
     app.register_blueprint(pet.bp)
     from . import fact
     app.register_blueprint(fact.bp)
 
     return app
+
+
+            
+
+
 
--- petfax/pet.py
+++ petfax/pet.py
@@ -8,11 +8,11 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index():  
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
 
 @bp.route('<int:pet_id>')
 def show(pet_id):
     pet = pets[pet_id-1]
-    return render_template('show_pet.html', pet=pet)
+    return render_template('pets/show_pet.html', pet=pet)
  
